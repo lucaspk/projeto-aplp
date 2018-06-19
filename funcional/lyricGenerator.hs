b667c8a98c50b9fea4d99b0df3c0432d7582926c
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-06-19 03:40:35 -0300

    Criação de funções de split, reclace e zip

--- funcional/lyricGenerator.hs
+++ funcional/lyricGenerator.hs
@@ -1,31 +1,66 @@
 import System.Environment
 import Txts
+import Data.Char
+import qualified Data.Map.Strict as Map
 
 main :: IO ()
 main = do
-    Txts.welcome
-    Txts.songGenre
+    --Txts.welcome
+    --Txts.songGenre
 
-    genre <- getLine
+    --genre <- getLine
 
     Txts.firstWord
 
     firstWord <- getLine
 
-    Txts.lastWord
+    --Txts.lastWord
 
-    lastWord <- getLine
+    --lastWord <- getLine
 
-    Txts.wordsNumber
+    --Txts.wordsNumber
 
-    wordsNumber <- getLine
+    --wordsNumber <- getLine
 
     popLyrics <- readFile "pop.txt"
     rapLyrics <- readFile "rap.txt"
 
     Txts.ready
+    --Replace '\n' to ' \n'
+    let part1 = replace rapLyrics
+    --let linesOfFiles = lines rapLyrics
 
-    print (take 50 popLyrics)
+    --let lowerCaseLines = [(toLowerCase line) | line <- linesOfFiles]
+
+    --Lower case every char
+    let part2 = toLowerCase part1
+
+    -- Split by ' '
+    let part3 = split (==' ') part2
+    
+    -- Zip a list into 2 lists
+    let part4 = myZip (tail part3) (init part3)
+    
+
+toLowerCase :: [Char] -> [Char]
+toLowerCase str = [ toLower x | x <- str]
+
+replace :: [Char] -> [Char]
+replace [] = []
+replace (x:xs) =
+    if x == '\n'
+      then " \n" ++ replace xs
+      else x : replace xs
 
 
+split :: (Char -> Bool) -> String -> [String]
+split p s = case dropWhile p s of
+            "" -> []
+            s' -> w : split p s''
+                where (w, s'') = break p s'
 
+
+myZip [] [] = []
+myZip _ [] = []
+myZip [] _ = []
+myZip (x:xs) (y:ys) = [(x,y)] ++ myZip xs ys
