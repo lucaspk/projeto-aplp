d445d15c5e0ec32d87be34b0e3cc2e1dca8015a9
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-06-23 18:54:16 -0300

    ProbDic quase finalizado

--- funcional/lyricGenerator.hs
+++ funcional/lyricGenerator.hs
@@ -42,46 +42,43 @@ main = do
     -- Zip a list into 2 lists
     let part4 = myZip (tail part3) (init part3)
 
-    --let meuMapSub = SubMap2 (Map.singleton "casa" 1)
-    let mapDic = Map.singleton "default" (Map.singleton "default" 1)
-    --print (subMap (Map.singleton "default" (Map.singleton "default" 1)) "default")
-    --print (Map.insert "default2" (Map.singleton "default2" 1) mapDic)
-    --print (Map.keys (Map.elems (Map.singleton "default" (Map.singleton "default2" 1))))
-    let test = (addAllToMap mapDic (take 500 part4))
-    print (test)
+    let mapDicInitial = Map.singleton "default" (Map.singleton "default" 1)
+
+    let mapDic = (addAllToMap mapDicInitial part4)
+
+    let freqProbInitial = Map.singleton "default" (Map.singleton "default" 1)
+    print freqProbInitial
+
+    let probDic = calculeAllProbDic (Map.toList mapDic) freqProbInitial
+
+    print (probDic)
+
+addProbs :: [(String, Int)] -> Int -> Map String Int -> Map String Int
+addProbs [] sum map = map
+addProbs (x:xs) sum map = addProbs xs sum (Map.insert (fst x) (sum) map)
+
+calculeMapSum :: [(String, Int)] -> Int
+calculeMapSum list = sum [snd x | x <- list]
+
+calculeProbDic :: Map String (Map String Int) -> (String, (Map String Int)) -> Map String (Map String Int)
+calculeProbDic map value = Map.insert (fst value) (addProbs (Map.toList (snd value)) (calculeMapSum (Map.toList (snd value))) (Map.singleton "default" 1)) map
+
+calculeAllProbDic :: [(String, (Map String Int))] -> Map String (Map String Int) -> Map String (Map String Int)
+calculeAllProbDic [] map = map
+calculeAllProbDic (x:xs) map = calculeAllProbDic xs (calculeProbDic map x) 
 
 subMap :: Map String (Map String Int) -> String -> Maybe (Map String Int)
 subMap map key = Map.lookup key map
 
---mapInsert :: Map String (Map String Int) -> (String, String) -> Map String Int
---mapInsert map value = Map.insert (snd value) 1 (subMap map (fst value))
-
---addToMap :: MapDic String b -> [(String, String)] -> Bool
---addToMap map (x:xs) = if not (Map.member (fst x) map) then False
---                        else True
 
 addToMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
 addToMap map value = if (Map.notMember (fst value) map) then Map.insert (fst value) (Map.singleton (snd value) 1) map
-                    --else ifMap.insert (snd value) (Map.singleton (snd value) 1) (Map.lookup (fst value) map)
                     else
-                        --if (Map.member (snd value) (Map.elems )) then
-                       --     Map.insert (fst value) ((fst value) (Map.singleton (snd value) 1)) map
-                        --else
-                          --  if (Map.notMember (snd value) (Map.findWithDefault "not" (fst value) map)) then
-                                --Map.insert (snd value) (Map.singleton (snd value) 1) map
-                            --else
                         if (isMember (findSubMap map (fst value)) (snd value)) then
                             updateSubMap map value
                         else
                             Map.insert (fst value) (Map.singleton (snd value) 1) map
-                                {-addToMap :: Maybe Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
-addToMap map value = if (Map.notMember (fst value) map) then Map.insert (fst value) (Map.singleton (snd value) 1) map
-                     else
-                        if (Map.notMember (snd value) (Map.lookup (fst value) map)) then
-                            Map.update (Map.insert (snd value) (Map.singleton (snd value) 1) (Map.lookup (fst value) map)) (fst value) map
-                        else 
-                            Map.update (Map.insertWith (+) (snd value) 1 (Map.lookup (fst value) map)) (fst value) map
--}
+                        
 
 updateSubMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
 updateSubMap map value = Map.insert (fst value) (Map.insert (snd value) (getValueSubMap map value) (findSubMap map (fst value))) map
@@ -121,18 +118,3 @@ myZip [] [] = []
 myZip [] _ = []
 myZip _ [] = []
 myZip (x:xs) (y:ys) = [(x,y)] ++ myZip xs ys
-
---addNewKey :: Key -> Key -> MainMap
---addNewKey k1 k2 = M.singleton k1 (M.singleton k2 1)
-
---uptdateKey :: (MainMap) -> Key -> Key -> SubMap
---updateKey map k1 k2 = alter 2 "joao" (fromListWithKey "casa" map)
-
---addFrequency (a, b) map = if (not (M.member a b map)) then True else False
-
---data SubMap2 a = SubMap2 (Map a Int) deriving (Eq, Show)
-
--- Insert
---insert mainKey (SubMap2 a) (SuperMap2 b) = SuperMap2 (Map.insertWith' mainKey a b)
-
---insertSub elem (SubMap2 b) = SubMap2 (Map.insertWith' (+) elem 1 b)
