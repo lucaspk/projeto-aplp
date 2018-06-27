eeb7c71b49f89eb1d9357276293b408703b57dbf
BrenoSouza <jose.souza@ccc.ufcg.edu.br>
2018-06-27 18:25:47 -0300

    read int

--- funcional/lyricGenerator.hs
+++ funcional/lyricGenerator.hs
@@ -1,105 +1,116 @@
 import System.Environment
 import Txts
+import Data.List 
 import Data.Char
-import qualified Data.Map.Strict as M
 import Data.Map (Map)
 import qualified Data.Map as Map
 
-data MapDic a b = MapDic (Map b (Map a Int)) deriving (Eq, Show)
 
+data MapDic a b = MapDic (Map b (Map a Int)) deriving (Eq, Show)
 
 main :: IO ()
 main = do
-    --Txts.welcome
-    --Txts.songGenre
-    --genre <- getLine
+    Txts.welcome
+    
+    Txts.songGenre
+    genre <- getLine
 
     Txts.firstWord
-
     firstWord <- getLine
-
-    --Txts.lastWord
-    --lastWord <- getLine
-    --Txts.wordsNumber
-    --wordsNumber <- getLine
 
     popLyrics <- readFile "pop.txt"
     rapLyrics <- readFile "rap.txt"
 
+    let pop = popLyrics :: String
+    let rap = rapLyrics :: String
+
+    Txts.lastWord
+    lastWord <- getLine
+
+    Txts.wordsNumber
+    wordsNum <- getLine
+    let wordsNumber = read wordsNum :: Int
+
     Txts.ready
-    --Replace '\n' to ' \n'
-    let part1 = replace rapLyrics
-    
-    --let linesOfFiles = lines rapLyrics
-    --let lowerCaseLines = [(toLowerCase line) | line <- linesOfFiles]
 
-    --Lower case every char
-    let part2 = toLowerCase part1
-
-    -- Split by ' '
-    let part3 = split (==' ') part2
+    let part3 = mySplit (==' ') (toLowerCase (replace rap))
     
-    -- Zip a list into 2 lists
     let part4 = myZip (tail part3) (init part3)
 
-    let mapDic = (addAllToMap Map.empty part4)
+    let mapDict = (addAllToMap Map.empty part4)
 
-    let probDic = calculeAllProbDic (Map.toList mapDic) Map.empty
+    let probDict = calculeAllProbDict (Map.toList mapDict) Map.empty
 
-    print (probDic)
+    let arrayStrings = (makeLyric probDict wordsNumber [firstWord])
+    
+    putStrLn (intercalate " " arrayStrings)
 
-divid :: Int -> Int -> Float
-divid a b = (fromIntegral a) / (fromIntegral b)
+    
+------------------------MakeRap--------------------------------
 
-addProbs :: [(String, Int)] -> Int -> Map String Float -> Map String Float
-addProbs [] sum map = map
-addProbs (x:xs) sum map = addProbs xs sum (Map.insert (fst x) (divid (snd x) sum) map)
+makeLyric :: Map String (Map String Float) -> Int -> [String] -> [String]
+makeLyric probDict 0 array = array
+makeLyric probDict wordsNumber array = makeLyric probDict (wordsNumber - 1) (array ++ [(markovNext (last array) probDict)])
+
+
+----------------------Markov_Next------------------------------
+markovNext :: String -> Map String (Map String Float) -> String
+markovNext current probDict = if (Map.member current probDict) then
+                                    getWord (probDict Map.! current)
+                              else "notOK"
+
+getWord :: Map String Float -> String
+getWord subMap = fst (last (Map.toList subMap))
+
+------------------------ToDict---------------------------------
+
+calculeAllProbDict :: [(String, (Map String Int))] -> Map String (Map String Float) -> Map String (Map String Float)
+calculeAllProbDict [] map = map
+calculeAllProbDict (x:xs) map = calculeAllProbDict xs (calculeProbDict map x) 
+
+calculeProbDict :: Map String (Map String Float) -> (String, (Map String Int)) -> Map String (Map String Float)
+calculeProbDict map (key, subMap) = Map.insert key (addProbs (Map.toList subMap) (calculeMapSum (Map.toList subMap)) (Map.empty)) map
 
 calculeMapSum :: [(String, Int)] -> Int
 calculeMapSum list = sum [snd x | x <- list]
 
-calculeProbDic :: Map String (Map String Float) -> (String, (Map String Int)) -> Map String (Map String Float)
-calculeProbDic map value = Map.insert (fst value) (addProbs (Map.toList (snd value)) (calculeMapSum (Map.toList (snd value))) (Map.empty)) map
+addProbs :: [(String, Int)] -> Int -> Map String Float -> Map String Float
+addProbs [] sum map = map
+addProbs (x:xs) sum map = addProbs xs sum (Map.insert (fst x) (divide (snd x) sum) map)
 
-calculeAllProbDic :: [(String, (Map String Int))] -> Map String (Map String Float) -> Map String (Map String Float)
-calculeAllProbDic [] map = map
-calculeAllProbDic (x:xs) map = calculeAllProbDic xs (calculeProbDic map x) 
+divide :: Int -> Int -> Float
+divide a b = (fromIntegral a) / (fromIntegral b)
 
-subMap :: Map String (Map String Int) -> String -> Maybe (Map String Int)
-subMap map key = Map.lookup key map
-
-
-addToMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
-addToMap map value = if (Map.notMember (fst value) map) then 
-                        Map.insert (fst value) (Map.singleton (snd value) 1) map
-                    else
-                        if (isMember (findSubMap map (fst value)) (snd value)) then
-                            updateSubMap map value
-                        else
-                            addNewToSubMap map value       
-
-addNewToSubMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
-addNewToSubMap map (key, value) = Map.insert key (Map.insert value 1 (Map.findWithDefault Map.empty key map)) map
-
-updateSubMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
-updateSubMap map value = Map.insert (fst value) (Map.insert (snd value) (getValueSubMap map value) (findSubMap map (fst value))) map
-
-getValueSubMap :: Map String (Map String Int) -> (String, String) -> Int
-getValueSubMap map value = (Map.findWithDefault 0 (snd value) (Map.findWithDefault (Map.singleton "test" 0) (fst value) map)) + 1
-
-findSubMap :: Map String (Map String Int) -> String -> Map [Char] Int
-findSubMap map key = Map.findWithDefault (Map.singleton "test" 2) key map
-
-isMember :: Map [Char] Int -> String -> Bool
-isMember map value = if (Map.member value map) then True else False
+---------------------------------------------------------------------------------------------------
 
 addAllToMap :: Map String (Map String Int) -> [(String, String)] -> Map String (Map String Int)
 addAllToMap map [] = map
 addAllToMap map (x:xs) = addAllToMap (addToMap map x) xs
 
+addToMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
+addToMap map (key, value) = if (Map.notMember key map) then 
+                        Map.insert key (Map.singleton value 1) map
+                    else
+                        if (Map.member value (findSubMap map key)) then
+                            updateSubMap map (key, value)
+                        else
+                            addNewToSubMap map (key, value)       
+
+addNewToSubMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
+addNewToSubMap map (key, value) = Map.insert key (Map.insert value 1 (map Map.! key)) map
+                            
+updateSubMap :: Map String (Map String Int) -> (String, String) -> Map String (Map String Int)
+updateSubMap map (key, value) = Map.insert key (Map.insert value (getValueSubMap map (key, value)) (findSubMap map key)) map
+
+getValueSubMap :: Map String (Map String Int) -> (String, String) -> Int
+getValueSubMap map (key, value) = ((map Map.! key) Map.! value ) + 1
+
+findSubMap :: Map String (Map String Int) -> String -> Map [Char] Int
+findSubMap map key = map Map.! key
+                            
+----------------- Funções auxiliares ---------------------
 toLowerCase :: [Char] -> [Char]
 toLowerCase str = [ toLower x | x <- str]
-
 
 replace :: [Char] -> [Char]
 replace [] = []
@@ -108,11 +119,10 @@ replace (x:xs) =
       then " \n" ++ replace xs
       else x : replace xs
 
-
-split :: (Char -> Bool) -> String -> [String]
-split p s = case dropWhile p s of
+mySplit :: (Char -> Bool) -> String -> [String]
+mySplit p s = case dropWhile p s of
             "" -> []
-            s' -> w : split p s''
+            s' -> w : mySplit p s''
                 where (w, s'') = break p s' 
 
 myZip [] [] = []
